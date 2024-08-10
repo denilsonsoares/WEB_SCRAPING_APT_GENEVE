19259e696cf75ade21839e47703c6dd42be271d6
denilsonsoares <166926902+denilsonsoares@users.noreply.github.com>
2024-08-10 12:36:44 -0300

    erro 429 resolvido com espera

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import time
 # Inicializando o cloudscraper
 scraper = cloudscraper.create_scraper()
 
-# Configurando o WebDriver
+# Configurando o WebDrivers
 chrome_options = Options()
 chrome_options.add_argument(
     "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
